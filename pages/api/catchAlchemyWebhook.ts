d03a71d85a14ffc3bbf51dd421f63ddd22ffb37b
poi-son-ivy <146914988+poi-son-ivy@users.noreply.github.com>
2024-10-30 14:33:04 -0700

    mr.hammond, the phones-
    mr.hammond, the alchemy webhook is working

--- pages/api/catchAlchemyWebhook.ts
+++ pages/api/catchAlchemyWebhook.ts
@@ -3,28 +3,26 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
         try {
-            // Get the body from the request
-            const postData = req.body;
+            // Check that the content type is correct for a GraphQL request
+            console.log(req.headers['content-type']);
+            if (req.headers['content-type'] !== 'application/json; charset=utf-8') {
+                return res.status(400).json({ error: 'Invalid content-type, expected application/json' });
+            }
 
-            // Enumerate all the POST variables
-            const enumeratedVars = Object.keys(postData).map((key) => {
-                return { [key]: postData[key] };
-            });
+            console.log(`Webhook ID: ${req.body.webhookId}`);
+            console.log(`id: ${req.body.id}`);
+            console.log(`createdAt: ${req.body.createdAt}`);
+            console.log(`type: ${req.body.type}`);
+            console.log(`event: `);
 
-            // Log the POST variables to the console for debugging
-            console.log('POST Variables:', enumeratedVars);
+            res.status(200).json({ message: 'Got webhook' });
 
-            // Send a response back to acknowledge receipt
-            res.status(200).json({
-                message: 'Webhook received',
-                postData: enumeratedVars,
-            });
         } catch (error) {
-            console.error('Error processing webhook:', error);
+            console.error('Error processing GraphQL webhook:', error);
             res.status(500).json({ error: 'Internal Server Error' });
         }
     } else {
-        // If not a POST request, return a 405 Method Not Allowed
+        // If it's not a POST request, return a 405 Method Not Allowed
         res.setHeader('Allow', ['POST']);
         res.status(405).json({ error: `Method ${req.method} Not Allowed` });
     }
