4e67a8b5d51c1cad10f810cb4714a218c694c206
poi-son-ivy <146914988+poi-son-ivy@users.noreply.github.com>
2024-10-31 13:13:55 -0700

    enrichment working- we were getting some results out of order so using .reduce to avoid concurrency badness

--- pages/api/catchAlchemyWebhook.ts
+++ pages/api/catchAlchemyWebhook.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { AlchemyUtils } from "@/pages/api/lib/AlchemyUtils";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
@@ -13,9 +14,33 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             console.log(`id: ${req.body.id}`);
             console.log(`createdAt: ${req.body.createdAt}`);
             console.log(`type: ${req.body.type}`);
-            console.log(`event: `);
+            //console.log(`event: ${JSON.stringify(req.body.event)}`);
+            const transactionData = JSON.parse(JSON.stringify(req.body.event));
 
-            res.status(200).json({ message: 'Got webhook' });
+            transactionData.activity.reduce(async (promiseChain:any, activity:any, index:any) => {
+                await promiseChain;
+
+                console.log(`Activity ${index + 1}:`);
+                console.log(`  Network: Base Mainnet`);
+                const fromAddress = await AlchemyUtils.resolveENS(activity.fromAddress) || 'None';
+                const toAddress = await AlchemyUtils.resolveENS(activity.toAddress) || 'None';
+
+                console.log(`  From Address: ${fromAddress}`);
+                console.log(`  To Address: ${toAddress}`);
+                console.log(`  Block Number: ${activity.blockNum || 'None'}`);
+                console.log(`  Transaction Hash: ${activity.hash || 'None'}`);
+                console.log(`  Value: ${activity.value || 'None'}`);
+                console.log(`  Asset: ${activity.asset || 'None'}`);
+                console.log(`  Category: ${activity.category || 'None'}`);
+                console.log(`  Raw Contract Address: ${activity.rawContract.address || 'None'}`);
+                console.log(`  Raw Contract Value: ${activity.rawContract.rawValue || 'None'}`);
+                console.log(`  Decimals: ${activity.rawContract.decimals || 'None'}`);
+                console.log('----------------------------------------');
+
+            }, Promise.resolve());
+
+
+            res.status(200).json({ message: 'Parsed transaction, thank you!' });
 
         } catch (error) {
             console.error('Error processing GraphQL webhook:', error);
